600bb9e96c5b71d4aee5fbe79386c6f3fdc91626
mac <mac@APPLEs-MacBook-Pro.local>
2025-01-05 17:31:13 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@
 import { Routes, Route } from "react-router-dom";
 import QuestionPreviewComponent from "./components/routes/auth_routes/QuestionPreviewComponent";
 import ThankyouPage from "./components/routes/auth_routes/ThankyouPage";
+import Home from "./components/rendarChildren/HomePage";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<QuestionPreviewComponent />} />
+        <Route path="" element={<Home />} />
+        <Route path="/response" element={<QuestionPreviewComponent />} />
         <Route path="/thankyou" element={<ThankyouPage />} />
       </Routes>
     </div>
